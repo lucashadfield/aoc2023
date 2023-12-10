40859f66335d42c79c3de064fc78f8bc17bb924d
Lucas <lucas@canva.com>
2023-12-11 10:26:29 +1100

    day03

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,6 +1,5 @@
+use std::collections::HashMap;
 advent_of_code::solution!(3);
-
-// pub fn check_part_number()
 
 pub fn part_one(input: &str) -> Option<u32> {
     let grid: Vec<Vec<char>> = input
@@ -9,31 +8,88 @@ pub fn part_one(input: &str) -> Option<u32> {
         .map(|s| s.chars().collect())
         .collect();
 
-    let mut part_num = String::new();
-    let mut is_part_adjacent = false;
+    let mut pending_part: Option<PendingPart> = None;
     let mut part_sum = 0;
 
     for i in 0..grid.len() {
         for j in 0..grid[i].len() {
-            match check_position(i, j, &grid, &part_num, is_part_adjacent) {
-                (Some(num), None) => {
-                    part_sum += num;
-                    part_num = String::new();
-                    is_part_adjacent = false;
+            match check_position(i, j, &grid, &pending_part) {
+                Some(FindResult::Pending(p)) => {
+                    pending_part = Some(p);
                 }
-                (None, Some(partial)) => {
-                    part_num = partial.part_num;
-                    is_part_adjacent = partial.is_part_adjacent;
+                Some(FindResult::Found(p)) => {
+                    part_sum += p.part_num;
+                    pending_part = None;
                 }
-                _ => continue,
+                _ => {
+                    pending_part = None;
+                }
             }
         }
     }
     Some(part_sum)
 }
 
-fn check_is_part_adjacent(row: usize, col: usize, grid: &Vec<Vec<char>>) -> Option<char> {
+pub fn part_two(input: &str) -> Option<u32> {
+    let grid: Vec<Vec<char>> = input
+        .trim()
+        .split('\n')
+        .map(|s| s.chars().collect())
+        .collect();
+
+    let mut pending_part: Option<PendingPart> = None;
+
+    let mut parts_map: HashMap<(usize, usize), Vec<Part>> = HashMap::new();
+
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            match check_position(i, j, &grid, &pending_part) {
+                Some(FindResult::Pending(p)) => {
+                    pending_part = Some(p);
+                }
+                Some(FindResult::Found(p)) => {
+                    parts_map.entry(p.location).or_insert_with(Vec::new).push(p);
+                    pending_part = None;
+                }
+                _ => {
+                    pending_part = None;
+                }
+            }
+        }
+    }
+
+    // filter to locations that have 2 parts
+    Some(
+        parts_map
+            .iter()
+            .filter(|(_, v)| v.len() == 2)
+            .map(|(_, v)| v[0].part_num * v[1].part_num)
+            .sum::<u32>(),
+    )
+}
+
+#[derive(Clone)]
+struct PendingPart {
+    is_valid: bool,
+    partial_part_num: String,
+    symbol: Option<char>,
+    location: Option<(usize, usize)>,
+}
+
+struct Part {
+    part_num: u32,
+    symbol: char,
+    location: (usize, usize),
+}
+
+enum FindResult {
+    Found(Part),
+    Pending(PendingPart),
+}
+
+fn check_is_part_adjacent(row: usize, col: usize, grid: &Vec<Vec<char>>) -> PendingPart {
     // check if this position has a part around it
+    let partial_part_num = grid[row][col].to_string();
     let start_row = row.checked_sub(1).unwrap_or(0);
     let end_row = usize::min(row + 1, grid.len() - 1);
     let start_col = col.checked_sub(1).unwrap_or(0);
@@ -43,72 +99,81 @@ fn check_is_part_adjacent(row: usize, col: usize, grid: &Vec<Vec<char>>) -> Opti
         for j in start_col..=end_col {
             if i != row || j != col {
                 match grid[i][j] {
-                    p if (!p.is_digit(10)) && (p != '.') => return Some(p),
+                    p if (!p.is_digit(10)) && (p != '.') => {
+                        return PendingPart {
+                            is_valid: true,
+                            partial_part_num: partial_part_num,
+                            symbol: Some(p),
+                            location: Some((i, j)),
+                        };
+                    }
                     _ => continue,
                 }
             }
         }
     }
-    None
+    PendingPart {
+        is_valid: false,
+        partial_part_num: partial_part_num,
+        symbol: None,
+        location: None,
+    }
 }
 
-struct PartialFind {
-    part_num: String,
-    is_part_adjacent: bool,
-}
-
-struct Find {
-    part_num: Option<u32>,
-    symbol: char,
-    location: (usize, usize),
-    partial_part_num: String,
-    is_part_adjacent: bool,
+fn merge_pending_parts(first: &PendingPart, second: &PendingPart) -> PendingPart {
+    PendingPart {
+        is_valid: first.is_valid || second.is_valid,
+        partial_part_num: format!("{}{}", first.partial_part_num, second.partial_part_num),
+        symbol: first.symbol.or(second.symbol),
+        location: first.location.or(second.location),
+    }
 }
 
 fn check_position(
     row: usize,
     col: usize,
     grid: &Vec<Vec<char>>,
-    part_num: &String,
-    is_part_adjacent: bool,
-) -> (Option<u32>, Option<PartialFind>) {
+    pending_part: &Option<PendingPart>,
+) -> Option<FindResult> {
     match grid[row][col] {
         p if p.is_digit(10) => {
-            let is_part_adjacent =
-                check_is_part_adjacent(row, col, grid).is_some() || is_part_adjacent;
-
-            if (col + 1 < grid[row].len() && !grid[row][col + 1].is_digit(10) && is_part_adjacent)
-                || (col + 1 == grid[row].len() && is_part_adjacent)
-            {
-                // found a valid part
-                (
-                    Some(format!("{}{}", part_num, p).parse::<u32>().unwrap()),
-                    None,
-                )
+            let mut updated_pending_part: PendingPart;
+            if pending_part.as_ref().map_or(false, |pp| pp.is_valid) {
+                // no need to check adjacency, just extend the PendingPart
+                updated_pending_part = pending_part.as_ref().unwrap().clone();
+                updated_pending_part.partial_part_num.push(p)
             } else {
-                // found a potential partial part
-                (
-                    None,
-                    Some(PartialFind {
-                        part_num: format!("{}{}", part_num, p),
-                        is_part_adjacent: check_is_part_adjacent(row, col, grid).is_some()
-                            || is_part_adjacent,
-                    }),
-                )
+                // there is no pending part, or it isn't yet validated
+                let adjacency = check_is_part_adjacent(row, col, grid);
+                updated_pending_part = match pending_part {
+                    Some(pp) => merge_pending_parts(&pp, &adjacency),
+                    None => adjacency,
+                };
+            }
+
+            // check if it's the last digit
+            let terminating_part = (col + 1 == grid[row].len()) || !grid[row][col + 1].is_digit(10);
+            if terminating_part {
+                if updated_pending_part.is_valid {
+                    // if the pending part is valid, convert to Part
+                    Some(FindResult::Found(Part {
+                        part_num: updated_pending_part
+                            .partial_part_num
+                            .parse::<u32>()
+                            .unwrap(),
+                        symbol: updated_pending_part.symbol.unwrap(),
+                        location: updated_pending_part.location.unwrap(),
+                    }))
+                } else {
+                    // otherwise, it's not a valid part, return None
+                    None
+                }
+            } else {
+                Some(FindResult::Pending(updated_pending_part))
             }
         }
-        // found nothing
-        _ => (
-            None,
-            Some(PartialFind {
-                part_num: String::new(),
-                is_part_adjacent: false,
-            }),
-        ),
+        _ => None,
     }
-}
-pub fn part_two(input: &str) -> Option<u32> {
-    None
 }
 
 #[cfg(test)]
@@ -124,6 +189,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(467835));
     }
 }
